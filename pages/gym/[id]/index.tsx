8dfbc97a1227607b2531afeb405ffe83e85c4e5d
Gonzalo Martinez <gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.com>
2023-01-04 05:48:39 -0300

    Prueba definitiva

--- pages/gym/[id]/index.tsx
+++ pages/gym/[id]/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { GetStaticPaths } from "next";
-import axios from "axios";
+
 import User from "../../../src/components/UserComponent/User";
 import { userAgent } from "next/server";
 
@@ -18,8 +18,10 @@ export default function UserData(props: any) {
 export const getStaticPaths: GetStaticPaths = async () => {
   // ...
   try {
-    const pt = "http://localhost:3000/api/gym/get-users";
-    const { data } = await axios.get(pt);
+    const pt = `http://${process.env.NEXT_PUBLIC_LOCAL_HOST}/api/gym/get-users`;
+    // const { data } = await axios.get(pt);
+    const res = await fetch(pt);
+    const data = await res.json();
     const paths = await data.map((user: any) => ({
       params: { id: String(user.id) },
     }));
@@ -32,8 +34,10 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export async function getStaticProps({ params }: any) {
   try {
-    const pt = `http://localhost:3000/api/gym/get-user?id=${params.id}`;
-    const { data } = await axios.get(pt);
+    const pt = `http://${process.env.NEXT_PUBLIC_LOCAL_HOST}/api/gym/get-user?id=${params.id}`;
+    // const { data } = await axios.get(pt);
+    const res = await fetch(pt);
+    const data = await res.json();
     return { props: { data } };
   } catch (error) {
     return { props: { data: false } };
