d8e6ed85dd4eac7585b494989409663475907edb
Gonzalo Martinez <gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.com>
2023-02-02 01:07:13 -0300

    handle-function tipado

--- src/components/AddUser/logic/handle-functions.ts
+++ src/components/AddUser/logic/handle-functions.ts
@@ -1,7 +1,4 @@
 import { Dispatch, SetStateAction } from "react";
-import { typesActivityGym } from "../../../../pages/gym";
-import { typesUser } from "../../../types/types-user";
-
 
 export function handleSelect(e: string, cbSet: Dispatch<SetStateAction<any>>) {
   cbSet(Number(e));
