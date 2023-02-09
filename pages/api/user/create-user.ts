1c89c3f72dbf2f4d97334ede6e999a6d7b5909c1
Gonzalo Martinez <gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.com>
2023-02-09 18:30:26 -0300

    info month

--- pages/api/user/create-user.ts
+++ pages/api/user/create-user.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { arrayWithNamesMonths } from "../../../services/infoMonths";
+import { arrayWithNamesMonths } from "../../../src/config/infoMonths";
 import { prisma } from "../../../services/prismaConfig";
 
 type Data = any;
