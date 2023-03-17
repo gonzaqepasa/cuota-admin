6c419096372aedfc94f52d4b7ccb08dfc20c8d04
Gonzalo Martinez <gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.com>
2023-03-17 01:55:56 -0300

    edit name a continuar

--- src/components/UserComponent/NameUser/NameUser.tsx
+++ src/components/UserComponent/NameUser/NameUser.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
+import { useState } from "react";
 import { firstLetterUpper } from "../../../logic/firstLetterUpper";
 import { fromNameToUrl } from "../../../logic/fromNameToUrl";
 import { selectColor } from "../../../logic/selectColor";
+import { VscEdit } from "react-icons/vsc";
 import { LinkDeptor } from "../../Deptor/LinkDeptor/LinkDeptor";
 import styles from "./NameUser.module.scss";
 
@@ -10,15 +12,47 @@ interface Props {
 }
 
 export const NameUser: React.FC<Props> = ({ user }) => {
+  const [name, setName] = useState(user.name);
+  const [editOn, setEditOn] = useState(true);
   return (
     <div className={styles.nameUserBox}>
-      <h2
-        style={{
-          borderBottom: `solid 2px ${selectColor(user.activity.nameActivity)}`,
-        }}
+      <div
+        className={`${editOn ? styles.inputContainer : styles.nameContainer}`}
       >
-        {firstLetterUpper(user.name)}
-      </h2>
+        {editOn ? (
+          <>
+            <input
+              style={{
+                borderBottom: `solid 2px ${selectColor(
+                  user.activity.nameActivity
+                )}`,
+              }}
+              className={styles.nameInput}
+              autoComplete={"none"}
+              type="text"
+            ></input>
+            <div>
+              <button>mas</button>
+              <button onClick={() => setEditOn(false)}>menos</button>
+            </div>
+          </>
+        ) : (
+          <>
+            <h2
+              style={{
+                borderBottom: `solid 2px ${selectColor(
+                  user.activity.nameActivity
+                )}`,
+              }}
+            >
+              {firstLetterUpper(name)}
+            </h2>
+            <button onClick={() => setEditOn(true)}>
+              <VscEdit />
+            </button>
+          </>
+        )}
+      </div>
       <Link
         style={{ color: selectColor(user.activity.nameActivity) }}
         href={`/list/${fromNameToUrl(user.activity.nameActivity)}`}
