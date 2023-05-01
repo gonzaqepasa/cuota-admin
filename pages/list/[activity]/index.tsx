f520077b8fb8688b03e2b6b7b74b4799a1201f7e
Gonzalo Martinez <gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.com>
2023-05-01 20:31:02 -0300

    a Continuar estylos

--- pages/list/[activity]/index.tsx
+++ pages/list/[activity]/index.tsx
@@ -74,7 +74,7 @@ export default function List() {
   if (error.msg) return <div className="main"> {error.msg}</div>;
   if (dataActivity.length !== 0)
     return (
-      <div className="main">
+      <div className="main hunt2">
         <Title activityName={String(activity)} />
 
         {dataActivity && (
