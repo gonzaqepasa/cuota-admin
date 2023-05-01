f04568b5bde970069ef201db81caa29ec59265ae
Gonzalo Martinez <gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.com>
2023-05-01 20:01:26 -0300

    Notification bug resolve

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -16,9 +16,9 @@ interface Props {
 export const Notification: React.FC<Props> = ({ activity, getDataAgain }) => {
   const [userDebtor, setUserDebtor] = useState<typesMonth[]>([]);
   const [view, setView] = useState<boolean>(false);
-  const random = Math.floor(Math.random() * userDebtor.length);
-  let userDebtorRandom = userDebtor[random];
-  console.log(userDebtor);
+
+  const [userDebtorRandom, setUserDeptorRandom] = useState<typesMonth>();
+  // console.log(userDebtor);
   const route = useRouter();
 
   useEffect(() => {
@@ -33,7 +33,10 @@ export const Notification: React.FC<Props> = ({ activity, getDataAgain }) => {
             month.calendar.User.activity.nameActivity === activity &&
             month.calendar.User.active === true
         );
+        const random = Math.floor(Math.random() * userDebtorActivity.length);
         setUserDebtor(userDebtorActivity);
+        setUserDeptorRandom(userDebtorActivity[random]);
+        console.log("Esto no debe pasar cuando abro y cierro addUser");
       } catch (err) {
         console.log(err);
       }
@@ -41,16 +44,18 @@ export const Notification: React.FC<Props> = ({ activity, getDataAgain }) => {
   }, [activity]);
 
   useEffect(() => {
+    const random = Math.floor(Math.random() * userDebtor.length);
+    setUserDeptorRandom(userDebtor[random]);
     const timeout = setTimeout(() => {
       // console.log("holasdasdasdasd");
       setView(true);
-    }, 30000);
+    }, 5000);
     return () => clearTimeout(timeout);
   }, [view]);
 
   return (
     <div className={`fixed bottom-5 left-5 `}>
-      {userDebtor.length > 0 && (
+      {userDebtor.length > 0 && userDebtorRandom?.id && (
         <div className=" backg-animate rounded-lg mr-2">
           <div
             className={`relative ${
@@ -60,7 +65,6 @@ export const Notification: React.FC<Props> = ({ activity, getDataAgain }) => {
             <button
               onClick={() => {
                 setView(false);
-                userDebtorRandom = userDebtor[random];
               }}
               className={`absolute text-red-700 top-0 right-2 font-semibold flex justify-center`}
             >
