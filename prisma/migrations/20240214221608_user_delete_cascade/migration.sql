116d4c2eae3f8ba3db5a938c22bd2bf9efcacc66
Gonzalo Martinez <gonzalovam@hotmail.com>
2024-02-20 00:54:44 -0300

    fix error

--- prisma/migrations/20240214221608_user_delete_cascade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "User" DROP CONSTRAINT "User_activityId_fkey";
-
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_activityId_fkey" FOREIGN KEY ("activityId") REFERENCES "Activity"("id") ON DELETE CASCADE ON UPDATE CASCADE;
