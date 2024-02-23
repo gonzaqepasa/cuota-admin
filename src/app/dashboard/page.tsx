cfac46bddb10ff315f9363df90ba2caeb81c99e1
Gonzalo Martinez <gonzalovam@hotmail.com>
2024-02-23 01:33:13 -0300

    fix deploy 2

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,14 +4,14 @@ import ProviderAuth from "../ProviderAuth";
 import ProviderNextUi from "../ProviderNextUi";
 
 const DashboardPage = async () => {
-  // const res = await getAllActivitiesToDashboard();
+  const res = await getAllActivitiesToDashboard();
   // console.log("Esto es el fking res", res);
 
   return (
     <main className="flex flex-col min-h-screen items-center">
       <ProviderAuth>
-        <ProviderNextUi>      <></>   
-          {/* <PricesRender data={res} /> */}
+        <ProviderNextUi>
+          <PricesRender data={res} />
         </ProviderNextUi>
       </ProviderAuth>
     </main>
