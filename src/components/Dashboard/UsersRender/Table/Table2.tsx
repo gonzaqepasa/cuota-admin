d563bd7d33bab344f84370501f59690898245f1f
indomito gym <you@example.com>
2024-04-19 15:43:58 -0300

    new Table2

--- src/components/Dashboard/UsersRender/Table/Table2.tsx
+++ src/components/Dashboard/UsersRender/Table/Table2.tsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import { typesActivity, typesUser } from "../../../../types/types-user";
-import { Pagination } from "@nextui-org/react";
+import { Avatar, Pagination } from "@nextui-org/react";
+import PaymentCol from "./PaymentCol";
+import { BtnAddPay } from "../../../Payments/BtnAddPay/BtnAddPay";
+import ButtonSendWpp from "../../../UserComponent/InformationPanel/PhoneUser/ButtonSendWpp";
+import ButtonDeleteUser from "../../../UserComponent/Config/ButtonDeleteUser";
+import { firstLetterUpper } from "../../../../logic/firstLetterUpper";
+import { isUserWithinPaymentMonth } from "./logicPayment";
+import { getByLastPay } from "../../../Payments/BtnAddPay/logicPayments";
+import Link from "next/link";
 
 interface Props {
   users: typesUser[];
@@ -34,14 +42,52 @@ const Table2: React.FC<Props> = ({ activities, users }) => {
   const paginatedUsers = paginateUsers(users, currentPage, pageSize);
 
   return (
-    <div>
-      <h1>Usuarios</h1>
-      <ul>
+    <div className=" flex flex-col items-center gap-4 ">
+      <ul className="min-h-[50vh] overflow-x-auto">
         {paginatedUsers.map((user: typesUser, index: number) => (
-          <li key={index}>{user.name}</li> // Ajusta user.name según la estructura de tus datos de usuario
+          <li
+            key={index}
+            className={` w-screen px-4 flex gap-10 items-center justify-between ${
+              index % 2 === 0 && "bg-gray-500/20"
+            }`}
+          >
+            <Link
+              className="flex w-max items-center hover:translate-x-1   transition "
+              href={"/user/" + user._id}
+            >
+              <Avatar
+                className={` mr-1 ${
+                  isUserWithinPaymentMonth(getByLastPay(user)?.createdAt)
+                    ? "bg-green-600 shadow-green-900 shadow-inner "
+                    : "bg-primary-100"
+                } `}
+                size="sm"
+                name={firstLetterUpper(user.name)}
+              />
+              <p className="text-content1-200 w-max ">
+                {firstLetterUpper(user.name)}
+              </p>
+            </Link>
+            <div>
+              <PaymentCol activities={activities} user={user} />
+            </div>
+            <div className="flex items-center gap-1">
+              <BtnAddPay
+                userData={user}
+                size="sm"
+                variant="bordered"
+                content="Pagar"
+                color="success"
+              />
+              <ButtonSendWpp user={user} />
+              <ButtonDeleteUser userData={user} />
+            </div>
+          </li> // Ajusta user.name según la estructura de tus datos de usuario
         ))}
       </ul>
       <Pagination
+        showControls
+        variant="faded"
         total={Math.ceil(users.length / pageSize)}
         onChange={onPageChange}
       />
